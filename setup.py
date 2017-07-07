816f34c55add6bd75a17ea0d65d55f97dfafaba0
Sean Callaway <seancallaway@gmail.com>
2017-07-07 09:39:07 -0700

    Added setup files.

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+    setup.py
+    ~~~~~~~~
+
+    Package setup file for laughs.
+
+    :copyright: (c) 2017 by Sean Callaway.
+    :license: MIT, see LICENSE for more details.
+"""
+from setuptools import setup
+
+setup(name='laughs',
+      version='0.1.0',
+      description='Pulls jokes from various APIs',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Games/Entertainment :: Fortune Cookies' ],
+      url='https://github.com/seancallaway/laughs',
+      author='Sean Callaway',
+      author_email='seancallaway@gmail.com',
+      license='MIT',
+      packages=['laughs'],
+      zip_safe=False)
